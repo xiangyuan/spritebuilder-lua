5a1f6643d27c4edfbe3f7b4896c031132086384b
sleepers <sleepers.tang@gmail.com>
2015-03-16 22:59:24 +0800

    first commit

---
+++ sbluaexample/frameworks/cocos2d-x/cocos/scripting/lua-bindings/auto/api/TMXTilesetInfo.lua
@@ -0,0 +1,25 @@
+
+--------------------------------
+-- @module TMXTilesetInfo
+-- @extend Ref
+-- @parent_module cc
+
+--------------------------------
+-- 
+-- @function [parent=#TMXTilesetInfo] getRectForGID 
+-- @param self
+-- @param #unsigned int gid
+-- @return rect_table#rect_table ret (return value: rect_table)
+        
+--------------------------------
+-- 
+-- @function [parent=#TMXTilesetInfo] toTable 
+-- @param self
+-- @return map_table#map_table ret (return value: map_table)
+        
+--------------------------------
+-- js ctor
+-- @function [parent=#TMXTilesetInfo] TMXTilesetInfo 
+-- @param self
+        
+return nil
